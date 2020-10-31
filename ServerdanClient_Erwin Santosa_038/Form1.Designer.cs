a627ff5032a437472329d9c0f3c6fcbaa5b19d6d
Erwin Santosa <erwincoc232@gmail.com>
2020-10-31 15:15:30 +0700

    Edit Code

--- ServerdanClient_Erwin Santosa_038/Form1.Designer.cs
+++ ServerdanClient_Erwin Santosa_038/Form1.Designer.cs
@@ -48,20 +48,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(327, 109);
+            this.label2.Location = new System.Drawing.Point(337, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Server ON";
+            this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(256, 159);
+            this.label3.Location = new System.Drawing.Point(337, 154);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(218, 17);
+            this.label3.Size = new System.Drawing.Size(46, 17);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Klik OFF untuk Mematikan Server";
+            this.label3.Text = "label3";
             // 
             // button1
             // 
@@ -81,6 +81,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "OFF";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- ServerdanClient_Erwin Santosa_038/Form1.cs
+++ ServerdanClient_Erwin Santosa_038/Form1.cs
@@ -25,9 +25,9 @@ namespace ServerdanClient_Erwin_Santosa_038
             try
             {
                 hostObj = new ServiceHost(typeof(Matematika));
-                hostObj.Open();
-                Console.WriteLine("Server is Ready!!!");
-                Console.ReadLine();
+                hostObj.Close();
+                label2.Text = "Server ON";
+                label3.Text = "Klik OFF untuk Mematikan Server";
                 hostObj.Close();
             }
             catch (Exception ex)
@@ -41,6 +41,25 @@ namespace ServerdanClient_Erwin_Santosa_038
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Close();
+                label2.Text = "Server OFF";
+                label3.Text = "Klik ON untuk Mengaktifkan Server";
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
