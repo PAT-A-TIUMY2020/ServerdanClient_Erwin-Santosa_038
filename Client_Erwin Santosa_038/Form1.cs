f2fdb11df74bc81d4a6ee3526137a0aaf3ebdc94
Erwin Santosa <erwincoc232@gmail.com>
2020-10-31 14:51:22 +0700

    Add project files.

---
+++ Client_Erwin Santosa_038/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_Erwin_Santosa_038
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a + b;
+            label7.Text = c.ToString();
+
+            
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a - b;
+            label8.Text = c.ToString();
+
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a * b;
+            label9.Text = c.ToString();
+
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a / b;
+            label10.Text = c.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ServerdanClient_Erwin Santosa_038/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P2_038;
+
+
+namespace ServerdanClient_Erwin_Santosa_038
+{
+    static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
